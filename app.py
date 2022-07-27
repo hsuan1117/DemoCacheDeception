7fba9169d035e9b7c264b3659ffb22dac86b506b
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2022-07-27 13:39:50 +0800

    Added Form

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import json
+
 from flask import Flask, session, render_template, jsonify, redirect, url_for, request, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '208f3f25-bec7-4607-b452-516803969967'
 
 
 @app.route('/')
@@ -17,20 +20,34 @@ def login():
     })
 
 
+@app.route('/save', methods=['POST'])
+def save():
+    data = json.load(open('data/user.json', 'r'))
+    email = session['user']
+    print(email)
+    if data[email] is not None:
+        data[email] = data[email].update(request.form)
+    else:
+        data[email] = request.form
+    json.dump(data, open('data/user.json', 'w'))
+    return redirect('/profile')
+
+
 @app.route('/logout')
 def logout():
+    del session['user']
     return redirect(url_for('index'))
 
 
 @app.route('/', methods=['GET', 'POST'], defaults={'path': ''})
 @app.route('/<path:path>', methods=['GET', 'POST'])
 def catch_all(path):
-    if path.startswith('notes'):
+    if path.startswith('profile'):
         if 'user' not in session:
             flash('請先登入')
             return redirect(url_for('index'))
-
-        return render_template('notes.html', notes=[])
+        data = json.load(open('data/user.json', 'r'))
+        return render_template('profile.html', user=data.get(session['user']))
 
     return path
 
